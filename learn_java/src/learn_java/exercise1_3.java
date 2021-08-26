a2c78645bcdd350b867946a45be3cea31055989e
taikinakano <kabayaki3323@gmail.com>
2021-08-26 20:32:54 +0900

    chapter1

---
+++ learn_java/src/learn_java/exercise1_3.java
@@ -0,0 +1,13 @@
+package learn_java;
+
+public class exercise1_3 {
+	public static void main(String[] args) {
+	boolean a = true;
+	char b = '駒';
+	double c = 3.14;
+	long d = 314159265853979L;
+	String e = "ミナトの攻撃！敵に１５ポイントのダメージを与えた";
+	
+	System.out.println(c + d + e);
+	}
+}
---
+++ learn_java/src/learn_java/exersize1_2.java
@@ -0,0 +1,11 @@
+package learn_java;
+
+public class exersize1_2 {
+	public static void main (String[] args) {
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		System.out.println(c);
+	}
+	
+}
