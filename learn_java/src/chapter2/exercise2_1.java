1bd4ccafdb785298edff19193774766dc6620cff
taikinakano <kabayaki3323@gmail.com>
2021-08-27 16:47:11 +0900

    chapter2

---
+++ learn_java/src/chapter2/exercise2_1.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class exercise2_1 {
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 10;
+		int z = x + y;
+		String ans = "x+yは" + z;
+		System.out.println(ans);
+	}
+}
